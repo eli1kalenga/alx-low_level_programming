f7aff1fd0890aeb627497f4cb3b31fdc1b7941a3
Eliot <eli1kalenga@gmail.com>
2023-02-16 17:43:39 -0800

    print types size on computer being compiled

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - A program that print various types on the computer it is compiling
+ *
+ * Return: 0  (Success)
+ *
+ */
+int main(void)
+{
+
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+return (0);
+}
