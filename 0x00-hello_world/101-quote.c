debb204a357d662f72ec551589b1c43b58306875
Eliot <eli1kalenga@gmail.com>
2023-02-16 18:38:24 -0800

    print a line in unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * Coded by eliot Kalenga.
+ *
+ * main - A C program that prints a line to the standard Error.
+ *
+ * Return: 1 (Success).
+ *
+ */
+
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19/n", stdout);
+return (1);
+}
