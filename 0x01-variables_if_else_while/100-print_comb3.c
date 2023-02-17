f931332738cca6cce858c5560ee3cb7dc0bbf6cb
Eliot <eli1kalenga@gmail.com>
2023-02-17 12:41:11 -0800

    print coms 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	char ch;
+	int n, m;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (n = 48; n <= 56; n++)
 	{
-		putchar(ch);
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
